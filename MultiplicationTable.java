340347c0813e43818f5d1db7dce1534b2d8bc498
godhalaakhila <akhilareddyg55@gmail.com>
2025-01-30 12:45:21 +0530

    Add files via upload

---
+++ MultiplicationTable.java
@@ -0,0 +1,15 @@
+public class MultiplicationTable {
+
+    public static void main(String[] args) {
+        // Define the number for which the multiplication table is to be printed
+        int number = 5;  // You can change this value to print the table for a different number
+        
+        // Loop to print the multiplication table of the number
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(number + " x " + i + " = " + (number * i));
+        }
+    }
+}
+ 
+    
+
